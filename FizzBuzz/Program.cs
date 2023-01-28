90847fc4d07d94e480af70df0c0883070cc51e14
Abner Cunha <abnermcunha@gmail.com>
2023-01-28 20:08:30 -0300

    Teste HackerRank

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FizzBuzz {
+    class Program {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Result.fizzBuzz(i);
+            }
+        }
+    }
+
+
+    class Result {
+
+        /*
+         * Complete the 'fizzBuzz' function below.
+         *
+         * The function accepts INTEGER n as parameter.
+         */
+
+        public static void fizzBuzz(int n)
+        {
+
+            bool isDivisibleBy3 = n % 3 == 0;
+            bool isDivisibleBy5 = n % 5 == 0;
+            if (isDivisibleBy3 && isDivisibleBy5)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (isDivisibleBy3)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (isDivisibleBy5)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+    }
+}
+
+
+
