32e1c46746544281fe5f6be5b25b6131e0901993
Abner Cunha <abnermcunha@gmail.com>
2023-01-28 21:15:52 -0300

    linha

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -18,12 +18,6 @@ namespace FizzBuzz {
 
     class Result {
 
-        /*
-         * Complete the 'fizzBuzz' function below.
-         *
-         * The function accepts INTEGER n as parameter.
-         */
-
         public static void fizzBuzz(int n)
         {
 
